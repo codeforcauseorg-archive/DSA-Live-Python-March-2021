6ed44430d54fd85e848d775c10db749d536b88a7
KeenWarrior <anujgargcse@gmail.com>
2021-03-27 12:01:49 +0530

    l 4 sorting

---
+++ lecture-4/sorting/bubble.py
@@ -0,0 +1,18 @@
+li = [10, 78, 65, 45, 47, 8, 56, 34, 19]
+
+
+def swap(nums, src, tgt):
+    temp = nums[src]
+    nums[src] = nums[tgt]
+    nums[tgt] = temp
+
+def bubble(nums):
+
+    for row in range(0, len(nums)):
+        for idx in range(0, len(nums) - row - 1):
+            if nums[idx] > nums[idx + 1]:
+                swap(nums, idx, idx+1)
+
+bubble(li)
+
+print(li)
