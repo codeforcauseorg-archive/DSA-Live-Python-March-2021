384ffe9e1e4046f49a871e194b5d6009fd829929
KeenWarrior <anujgargcse@gmail.com>
2021-04-05 11:43:00 +0530

    L rec

--- lecture-07/equalSum.py
+++ lecture-07/equalSum.py
@@ -1,14 +1,13 @@
-def equalSum(complete, left=[], right=[]):
-    if(len(complete) == 0):
+def equalSum(complete, index=0,  left=[], right=[]):
+    if(len(complete) == index):
         if(sum(left) == sum(right)):
             print(left, right)
         return
 
-    current = complete[0]
-    remaining = complete[1:]
+    current = complete[index]
 
-    equalSum(remaining, left+[current], right)
-    equalSum(remaining, left, right+[current])
+    equalSum(complete, index+1, left+[current], right)
+    equalSum(complete, index+1, left, right+[current])
 
 
 equalSum([1, 2, 3])
