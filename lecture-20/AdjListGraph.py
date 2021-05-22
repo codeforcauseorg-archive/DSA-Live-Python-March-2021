63660488e9c66ad9b077b735ae5ad7b477079776
keenwarrior <anujgargcse@gmail.com>
2021-05-22 11:17:59 +0530

    l 20

--- lecture-20/AdjListGraph.py
+++ lecture-20/AdjListGraph.py
@@ -30,6 +30,85 @@ class Graph:
             print(vertex.value, end=" => ")
             print(", ".join([neighbour.value for neighbour in vertex.neighbours]))
 
+    def dft(self, value):
+        start = self.vertices[value]
+        stack = []
+        to_process = set()
+
+        stack.append(start)
+        to_process.add(start)
+
+        while len(stack) > 0:
+            top = stack.pop()
+            print(top.value)
+
+            for neighbour in top.neighbours:
+                if neighbour not in to_process:
+                    stack.append(neighbour)
+                    to_process.add(neighbour)
+
+    def dfs(self, value, target):
+        start = self.vertices[value]
+        stack = []
+        to_process = set()
+
+        stack.append(start)
+        to_process.add(start)
+
+        while len(stack) > 0:
+            top = stack.pop()
+            if top.value == target:
+                return True
+
+            for neighbour in top.neighbours:
+                if neighbour not in to_process:
+                    stack.append(neighbour)
+                    to_process.add(neighbour)
+
+        return False
+
+    def bft(self, value):
+        start = self.vertices[value]
+        queue = []
+        to_process = set()
+
+        queue.append(start)
+        to_process.add(start)
+
+        while len(queue) > 0:
+            top = queue.pop(0)
+            print(top.value)
+
+            for neighbour in top.neighbours:
+                if neighbour not in to_process:
+                    queue.append(neighbour)
+                    to_process.add(neighbour)
+
+    def min_dist_eq(self, value):
+        start = self.vertices[value]
+        queue = []
+        to_process = set()
+
+        queue.append(start)
+        to_process.add(start)
+        queue.append(None)
+        layer = 0
+
+        while len(queue) > 1:
+            top = queue.pop(0)
+
+            if not top:
+                queue.append(None)
+                layer += 1
+                continue
+
+            print(top.value, layer)
+
+            for neighbour in top.neighbours:
+                if neighbour not in to_process:
+                    queue.append(neighbour)
+                    to_process.add(neighbour)
+
 
 graph = Graph()
 graph.create_vertex("A")
@@ -42,7 +121,7 @@ graph.create_edge("C", "D")
 graph.create_edge("B", "D")
 graph.create_edge("A", "D")
 
-graph.display()
+graph.min_dist_eq("A")
 
 
 
